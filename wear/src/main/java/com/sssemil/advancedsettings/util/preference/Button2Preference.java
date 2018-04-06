bdde1d635b4b943c3866ebc11801074dc646f803
wzhy90 <wzhy90@gmail.com>
2018-04-06 23:14:23 +0800

    Add System built-in power menu to power menu activity.

--- wear/src/main/java/com/sssemil/advancedsettings/util/preference/Button2Preference.java
+++ wear/src/main/java/com/sssemil/advancedsettings/util/preference/Button2Preference.java
@@ -1,6 +1,8 @@
 package com.sssemil.advancedsettings.util.preference;
 
 import android.content.Context;
+import android.content.Intent;
+import android.provider.Settings;
 import android.util.AttributeSet;
 import android.util.Log;
 
@@ -27,7 +29,15 @@ public class Button2Preference extends Preference {
     public void onPreferenceClick() {
         try {
             if (!super.getActivity().equals("null")) {
-                Runtime.getRuntime().exec(String.valueOf(super.getActivity()));
+                switch (String.valueOf(super.getActivity())) {
+                    case "system_menu":
+                        Intent intent = new Intent(Settings.ACTION_PRIVACY_SETTINGS);
+                        mContext.startActivity(intent);
+                        break;
+                    default:
+                        Runtime.getRuntime().exec(String.valueOf(super.getActivity()));
+                        break;
+                }
             }
         } catch (IOException e) {
             Log.d(TAG, "catch " + e.toString() + " hit in run", e);
